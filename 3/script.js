52395d9e775ab32dfa7a16662b9a44f7dfc3b270
piyush rathore <piysuh260305@gmail.com>
2024-06-03 06:48:32 +0530

    little changes

--- 3/script.js
+++ 3/script.js
@@ -3,14 +3,16 @@ let tyear = document.querySelector(".elem4");
 let tday = document.querySelector(".elem1");
 let tmonth = document.querySelector(".elem2");
 
-function calender() {
-  var today = new Date(); // creates a new date object with the current date and time
 
-  var date = today.getDate(); // gets the day of the month (from 1 to 31)
-  console.log(date);
-  var day = today.getDay(); // gets the day of the week (from 0 to 6, 0 is Sunday)
-  var month = today.getMonth(); // gets the month (from 0 to 11, 0 is January)
-  var year = today.getFullYear(); // gets the year (four digits)
+
+function calender() {
+  let today = new Date(); // creates a new date object with the current date and time
+
+  let date = today.getDate(); // gets the day of the month (from 1 to 31)
+  
+  let day = today.getDay(); // gets the day of the week (from 0 to 6, 0 is Sunday)
+  let month = today.getMonth(); // gets the month (from 0 to 11, 0 is January)
+  let year = today.getFullYear(); // gets the year (four digits)
   tdate.innerHTML += date;
   tyear.innerHTML += year;
   let days = [
@@ -38,6 +40,13 @@ function calender() {
     "December",
   ];
   tmonth.innerHTML += months[month];
+
+
+
+  
 }
+
+
+
 
 calender();
